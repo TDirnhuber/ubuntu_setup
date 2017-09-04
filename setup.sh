902d3dcb3ed3907dfcc14e4e8aaff5ec3cc938a2
Tobias Dirnhuber <tdirnhuber@hotmail.co.uk>
2017-09-04 19:48:47 +0100

    First try

---
+++ setup.sh
@@ -0,0 +1,42 @@
+sudo apt-get install xcb \
+     libxcb-util0-dev \
+     libxcb-ewmh-dev \
+     libxcb-randr0-dev \
+     libxcb-icccm4-dev \
+     libxcb-keysyms1-dev \
+     libxcb-xinerama0-dev \
+     libasound2-dev \
+     gcc \
+     make \
+     libxcb-xtest0-dev \
+     libxft-dev \
+     libx11-xcb-dev \
+     mpd \
+     mpc \
+     texlive \
+     zathura \
+     unclutter \
+
+mkdir -p ~/install_temp
+cd  ~/install_temp
+git clone https://github.com/baskerville/bspwm.git
+git clone https://github.com/baskerville/sxhkd.git
+git clone https://github.com/baskerville/sutils.git
+git clone https://github.com/baskerville/xtitle.git
+git clone https://github.com/baskerville/xdo.git
+
+cd bspwm/ && make && sudo make install
+cd ../sxhkd/ && make && sudo make install
+cd ../sutils/ && make && sudo make install
+cd ../xtitle/ && make && sudo make install
+cd ../xdo/ && make && sudo make install
+
+cd ~ 
+git clone https://github.com/TDirnhuber/dotfiles.git
+cd dotfiles
+./mksymlnks
+sudo systemctl set-default multi-user.target
+reboot
+
+
+
